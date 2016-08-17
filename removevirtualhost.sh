a309702eecb60060e700b029b0722fd23d095a49
iamLos <carloscash.blog@gmail.com>
2016-08-16 22:58:28 -0400

    Update removevirtualhost.sh
    from apache2 to nginx

--- removevirtualhost.sh
+++ removevirtualhost.sh
@@ -8,8 +8,8 @@ done
 
 sudo a2dissite $hostname
 sudo rm -rf /var/www/$hostname
-sudo rm -f /etc/apache2/sites-available/$hostname.conf
+sudo rm -f /etc/nginx/sites-available/$hostname.conf
 sudo sed -i "/$hostname/d" /etc/hosts
-sudo service apache2 restart
+sudo service nginx restart
 
 echo "Your host $hostname has been successfully removed, you can type $hostname in browser and check this"
